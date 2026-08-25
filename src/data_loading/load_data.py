d38098b52a428f7a24a4283819fa11479f7b4835
Adriano <dsm.adriano@gmail.com>
2026-08-25 06:33:37 -0300

    DVC part 2

--- src/data_loading/load_data.py
+++ src/data_loading/load_data.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ logger = logging.getLogger("src.data_loading.load_data")
 
 
 def fetch_data() -> pd.DataFrame:
-    """Fetch the breast cancer dataset and convert to DataFrame.
+    """Fetch the breast cancer dataset and convert to DataFrame
 
     Returns:
         pd.DataFrame: DataFrame containing the breast cancer data with features and target
@@ -27,6 +28,9 @@ def fetch_data() -> pd.DataFrame:
     
     # Target column
     data["target"] = dataset.target
+
+    # Shuffle data to simulate chanced data
+    data = data.sample(frac=1, random_state=int(time.time())).reset_index(drop=True)
     
     return data
 
